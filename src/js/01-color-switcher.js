9d23ae6a6cb5ff5952b4412f019383f875192fed
VolodymyrSkaskiv <skaskivvolodymyr@gmail.com>
2023-02-19 00:15:46 +0200

    Update 01-color-switcher.js
    disabled button start on click start button

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -8,20 +8,20 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-refs.start.addEventListener('click', onChangeBgColor);
-refs.stop.addEventListener('click', offChangeBgColor);
+refs.start.addEventListener('click', startChangeBgColor);
+refs.stop.addEventListener('click', stopChangeBgColor);
 
-function onChangeBgColor() {
+function startChangeBgColor() {
   console.log('onChangeBgColor');
-  idInterval = setInterval(
-    (document.body.style.backgroundColor = getRandomHexColor()),
-    1000
-  );
-  //   document.body.style.backgroundColor = getRandomHexColor();
-  // setInterval({body.bgColor.value = getRandomHexColor()},1000)
+  refs.start.disabled = true;
+  idInterval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
 }
 
-function offChangeBgColor() {
+function stopChangeBgColor() {
+  refs.start.disabled = false;
+
   clearInterval(idInterval);
   console.log('offChangeBgColor');
 }
