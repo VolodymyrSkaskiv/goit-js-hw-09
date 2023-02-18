0210e5d4bef63a4a238d9a97023ea1cec41591df
VolodymyrSkaskiv <skaskivvolodymyr@gmail.com>
2023-02-18 23:58:58 +0200

    add change color
    add click button => change bgcolor

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
+let idInterval;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.start.addEventListener('click', onChangeBgColor);
+refs.stop.addEventListener('click', offChangeBgColor);
+
+function onChangeBgColor() {
+  console.log('onChangeBgColor');
+  idInterval = setInterval(
+    (document.body.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+  //   document.body.style.backgroundColor = getRandomHexColor();
+  // setInterval({body.bgColor.value = getRandomHexColor()},1000)
+}
+
+function offChangeBgColor() {
+  clearInterval(idInterval);
+  console.log('offChangeBgColor');
+}
