7f75c1347f68392c6beacda59ecd4adb9bc8dd8b
VolodymyrSkaskiv <skaskivvolodymyr@gmail.com>
2023-02-20 22:09:59 +0200

    install and add notiflix

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,14 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+import Notiflix from 'notiflix';
+
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { Report } from 'notiflix/build/notiflix-report-aio';
+// import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
+// import { Loading } from 'notiflix/build/notiflix-loading-aio';
+// import { Block } from 'notiflix/build/notiflix-block-aio';
+
 const refs = {
   input: document.querySelector('#datetime-picker'),
   start: document.querySelector('[data-start]'),
@@ -36,7 +44,72 @@ class Timer {
   }
 }
 
-flatpickr('input#datetime-picker', {});
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr('input#datetime-picker', options);
 refs.input.addEventListener('input', inputDateTime);
 
 function inputDateTime(date) {}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+console.log(convertMs(2000));
+
+function addLeadingZero(value) {
+  return toString(value).padStart(2, '0');
+}
+
+console.log(addLeadingZero(1));
+
+Notiflix.Notify.success('Sol lucet omnibus');
+
+Notiflix.Notify.failure('Qui timide rogat docet negare');
+
+Notiflix.Notify.warning('Memento te hominem esse');
+
+Notiflix.Notify.info('Cogito ergo sum');
+
+// e.g. Message with a callback
+Notiflix.Notify.success('Click Me', function cb() {
+  // callback
+});
+
+// e.g. Message with the new options
+Notiflix.Notify.success('Click Me', {
+  timeout: 6000,
+});
+
+// e.g. Message with callback, and the new options
+Notiflix.Notify.success(
+  'Click Me',
+  function cb() {
+    // callback
+  },
+  {
+    timeout: 4000,
+  }
+);
