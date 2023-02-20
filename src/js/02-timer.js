2bbcf21db05adad751a86b5a97e7a9dd0f9fd198
VolodymyrSkaskiv <skaskivvolodymyr@gmail.com>
2023-02-20 12:27:37 +0200

    add class Timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,42 @@
 import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
-// If using flatpickr in a framework, its recommended to pass the element directly
-flatpickr(element, {});
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  start: document.querySelector('[data-start]'),
+};
 
-// Otherwise, selectors are also supported
-flatpickr('#myID', {});
+class Timer {
+  constructor({ onTick }) {
+    this.intervalId = null;
+    this.isActive = false;
+    this.onTick = onTick;
+  }
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    const startTime = Date.now();
+    this.isActive = true;
+
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = currentTime - startTime;
+      const time = this.getTimeComponents(deltaTime);
+
+      this.onTick(time);
+    }, 1000);
+  }
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+
+    const time = getTimeComponents(0);
+    this.onTick(time);
+  }
+}
+
+flatpickr('input#datetime-picker', {});
+refs.input.addEventListener('input', inputDateTime);
+
+function inputDateTime(date) {}
