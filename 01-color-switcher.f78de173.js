e3a79b71bf9263a70b08d69d8580db80b576ca4d
VolodymyrSkaskiv <106022138+VolodymyrSkaskiv@users.noreply.github.com>
2023-02-22 21:59:51 +0000

    Deploying to gh-pages from @ VolodymyrSkaskiv/goit-js-hw-09@babeae02c1f3d5c100d849ae8ad5c5b2427c44dd 🚀

---
+++ 01-color-switcher.f78de173.js
@@ -0,0 +1,2 @@
+!function(){const t={start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]")};let e;t.start.addEventListener("click",(function(){t.start.disabled=!0,e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stop.addEventListener("click",(function(){t.start.disabled=!1,clearInterval(e)}))}();
+//# sourceMappingURL=01-color-switcher.f78de173.js.map
